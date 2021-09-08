d2724d79b116baadd9585f79855e4d125a11f748
Saam <saamjwesley@gmail.com>
2021-09-08 23:42:30 +0530

    before tmdb addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Route } from "react-router-dom";
+//axios 
+import axios from "axios";
+
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
 import MovieHOC from "./HOC/Movie.HOC";
@@ -6,6 +8,11 @@ import MovieHOC from "./HOC/Movie.HOC";
 import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
 import Plays from "./pages/Plays.page";
+
+//axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 // Import css files
 import "slick-carousel/slick/slick.css";
