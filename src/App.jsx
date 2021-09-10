9d458f5d45f3941d60c4804b4455c2af7b3fe0bb
Saam <saamjwesley@gmail.com>
2021-09-10 14:30:24 +0530

    TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//axios 
 import axios from "axios";
 
 // HOC
@@ -9,14 +8,16 @@ import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
 import Plays from "./pages/Plays.page";
 
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 //axios default settings
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 axios.defaults.params = {};
 axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 function App() {
   return (
     <>
